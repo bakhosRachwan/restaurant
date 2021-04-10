7be2f4eace37c85a5d2d70c637627fdca56a4337
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-04-10 19:05:22 +0300

    Build: chakra ui config

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import { ChakraProvider } from "@chakra-ui/react"
+import  Theme  from "./components/Colors/Colors"
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={Theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
