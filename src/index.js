b40aafbe29768c1f0352d71f6cc5f113b767d833
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-04-15 01:31:18 +0300

    Feat: context api

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import { ChakraProvider } from "@chakra-ui/react"
 import  Theme  from "./components/Colors/Colors"
+import { StateProvider } from './State';
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider theme={Theme}>
-      <App />
-    </ChakraProvider>
+    <StateProvider>
+      <ChakraProvider theme={Theme}>
+        <App />
+      </ChakraProvider>
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
